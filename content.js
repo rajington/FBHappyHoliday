388181e9df9a90fb3e19331e30f417f256990f4e
Rajarshi Nigam <gitlab@raj.oib.com>
2016-05-18 22:03:19 -0400

    refactoring and documentation

--- content.js
+++ content.js
@@ -1,18 +1,36 @@
-let clickedEl;
+// we must remember the last element we clicked so we know where to insert the message
+let clickedTextArea;
 
-document.addEventListener('mousedown', ({ button, target }) => {
+// mouse event listener that persists the element clicked
+function rememberRightClickedElement({ button, target }) {
   // right click
   if (button === 2) {
-    clickedEl = target;
+    clickedTextArea = target;
   }
-}, true);
+}
 
+// gets the available birthday message boxes
+const nodes = document.querySelectorAll('#events_birthday_view textarea');
+
+// adds presisting listeners to them
+for (const textarea of Array.from(nodes)) {
+  textarea.addEventListener('mousedown', rememberRightClickedElement);
+}
+
+// get the first name from the text area using vanilla js
+function getFirstNameFromTextArea(textarea) {
+  const ancestor = textarea.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode
+    .parentNode.parentNode;
+  const nameNode = ancestor.querySelector('a');
+  const firstName = nameNode.text.split(' ')[0];
+  return firstName;
+}
+
+// create the contextual menu by sending a default message
 chrome.runtime.sendMessage(true, () => {
+  // listen for contextual menu selections
   chrome.runtime.onMessage.addListener(holiday => {
-    const ancestor = clickedEl.parentNode.parentNode.parentNode.parentNode
-      .parentNode.parentNode.parentNode.parentNode;
-    const nameNode = ancestor.querySelector('a');
-    const firstName = nameNode.text.split(' ')[0];
-    clickedEl.value = `Happy ${holiday} ${firstName}!`;
+    const firstName = getFirstNameFromTextArea(clickedTextArea);
+    clickedTextArea.value = `Happy ${holiday} ${firstName}!`;
   });
 });
