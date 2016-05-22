6a2c2f8632e718ffcb89139e9473240737dc7472
Rajarshi Nigam <gitlab@raj.oib.com>
2016-05-22 12:09:34 -0400

    allow butt click to launch tab

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.browserAction.onClicked.addListener(() => chrome.tabs.create({
+  // launch the URL (from the manifest to avoid duplication)
+  url: chrome.runtime.getManifest().content_scripts[0].matches[0],
+}));
   // format today's date for API
   const date = new Date();
   const monthDay = `${date.getMonth() + 1}/${date.getDate()}`;
