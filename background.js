4f76976ac3469569dfbb0e75d28ce63c1d68ad81
Rajarshi Nigam <gitlab@raj.oib.com>
2016-05-22 12:10:37 -0400

    keep a cache to prevent repeat requests

--- background.js
+++ background.js
@@ -1,34 +1,43 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 chrome.browserAction.onClicked.addListener(() => chrome.tabs.create({
   // launch the URL (from the manifest to avoid duplication)
   url: chrome.runtime.getManifest().content_scripts[0].matches[0],
 }));
+
+let cache = null;
+
+chrome.runtime.onMessage.addListener((request, sender) => {
+  // trigger sending context menu clicks to this tab as well
+  chrome.contextMenus.onClicked.addListener(({ menuItemId }) => {
+    chrome.tabs.sendMessage(sender.tab.id, menuItemId);
+  });
+
   // format today's date for API
   const date = new Date();
   const monthDay = `${date.getMonth() + 1}/${date.getDate()}`;
 
-  fetch(`https://www.checkiday.com/api.php?d=${monthDay}`)
-    .then(resp => resp.json())
-    .then(holidays => {
-      chrome.contextMenus.removeAll();
+  // if we haven't gotten data yet or for today
+  if (cache == null || cache !== monthDay) {
+    fetch(`https://www.checkiday.com/api.php?d=${monthDay}`)
+      .then(resp => resp.json())
+      .then(holidays => {
+        chrome.contextMenus.removeAll();
 
-      for (const holiday of holidays) {
-        chrome.contextMenus.create({
-          id: holiday,
-          title: holiday,
-          contexts: ['editable'],
-          documentUrlPatterns: [sender.url],
-        });
-      }
+        for (const holiday of holidays) {
+          chrome.contextMenus.create({
+            id: holiday,
+            title: holiday,
+            contexts: ['editable'],
+            documentUrlPatterns: [sender.url],
+          });
+        }
 
-      chrome.contextMenus.onClicked.addListener(({ menuItemId }) => {
-        chrome.tabs.sendMessage(sender.tab.id, menuItemId);
+        // we don't need to update
+        cache = monthDay;
       });
 
-      return holidays.length;
-    })
-    .then(length => sendResponse(length));
+    // return true to signify async
+    return true;
+  }
 
-  // return true to signify async
-  return true;
+  return false;
 });
